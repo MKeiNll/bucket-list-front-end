df7a8f9b5e8b412753e9ff60bfa795b04c85c7c1
MKeiNll <levgirich@gmail.com>
2019-04-01 18:15:36 +0300

    Fixed ability to create new entries

--- src/components/EntryCreationForm.tsx
+++ src/components/EntryCreationForm.tsx
@@ -1,32 +1,46 @@
 import * as React from "react";
 import "../styles/entryCreationForm.css";
 
-export const EntryCreationForm = ({ onSubmit }) => (
-  <div className="entryCreationForm">
-    <span className="title">NEW ENTRY</span>
-    <form
-      className="entry-form"
-      onSubmit={e => {
-        console.log(e.target);
-        e.preventDefault();
-        onSubmit(e.target.title.value, e.target.content.value);
-      }}
-    >
-      <input
-        className="title-input"
-        type="text"
-        placeholder="Title"
-        name="title"
-        required
-      />
-      <input
-        className="content-input"
-        type="text"
-        placeholder="Content"
-        name="content"
-        required
-      />
-      <input className="entry-submit" type="submit" value="SUBMIT" />
-    </form>
-  </div>
-);
+export class EntryCreationForm extends React.Component<any, any> {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.onSubmit(event.target.title.value, event.target.content.value);
+  }
+
+  render() {
+    let bookImage;
+    if (this.props.image != "") {
+      bookImage = <img className="bookImage" src={this.props.image} />;
+    } else {
+      bookImage = "";
+    }
+
+    return (
+      <div className="entryCreationForm">
+        <span className="title">NEW ENTRY</span>
+        <form className="entry-form" onSubmit={this.handleSubmit}>
+          <input
+            className="title-input"
+            type="text"
+            placeholder="Title"
+            name="title"
+            required
+          />
+          <input
+            className="content-input"
+            type="text"
+            placeholder="Content"
+            name="content"
+            required
+          />
+          <input className="entry-submit" type="submit" value="SUBMIT" />
+        </form>
+      </div>
+    );
+  }
+}
